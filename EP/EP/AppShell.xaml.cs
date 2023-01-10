f55775699cb0a63854a5a5f58fcf193a011715bf
Tehran Doroudchi <ali.doroudchi@interdisount.ch>
2023-01-10 17:46:59 +0100

    Create Project

---
+++ EP/EP/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EP;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ EP/EP/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace EP;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
